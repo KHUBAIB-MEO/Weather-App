1e6e753a3e9a8b0f5682acd649e10bdc069b815d
unknown <Khubaibnaeem42@gmail.com>
2024-11-25 03:00:31 +0500

    Weather App

--- lib/screens/weather.dart
+++ lib/screens/weather.dart
@@ -1,6 +1,10 @@
+//import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:weather_app/utils/color_constants.dart';
+import 'package:http/http.dart' as http;
 
 class Weather extends StatefulWidget {
   const Weather({super.key});
@@ -10,6 +14,67 @@ class Weather extends StatefulWidget {
 }
 
 class _WeatherState extends State<Weather> {
+  // ignore: prefer_typing_uninitialized_variables
+  var weatherData;
+  double temp = 0;
+  int tempInt = 0;
+  String? description;
+  double feelsLike = 0;
+  int feelsLikeInt = 0;
+  double windSpeed = 0;
+  int windSpeedInt = 0;
+  String cityName = " ";
+  String? bgColor;
+  String imageShow = "";
+  int? weatherId;
+
+  void imagePicker(int id, String backgroundColor) {
+    if (id == 200 || id == 211 || id == 232) {
+      imageShow = "assets/animation_widgets/thunderstorm.json";
+    } else if (id == 300 ||
+        id == 310 ||
+        id == 321 ||
+        id == 500 ||
+        id == 503 ||
+        id == 531) {
+      imageShow = "assets/animation_widgets/rain.json";
+    } else if (id == 800 && backgroundColor == "d") {
+      imageShow = "assets/animation_widgets/sunny clear day.json";
+    } else if (id == 801 || id == 804) {
+      imageShow = "assets/animation_widgets/cloudy.json";
+    } else if (id == 701 || id == 711 || id == 741) {
+      imageShow = "assets/animation_widgets/fog.json";
+    } else if (id == 800 && backgroundColor == "n") {
+      imageShow = "assets/animation_widgets/night clear.json";
+    }
+  }
+
+  void getWeatherData(String city) async {
+    String baseUrl =
+        "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=675556f33d7e1777e499f101eaf8310e";
+    Uri url = Uri.parse(baseUrl);
+    http.Response response = await http.get(url);
+    if (response.statusCode == 200) {
+      setState(() {
+        weatherData = json.decode(response.body);
+        temp = weatherData["main"]["temp"] - 273.15;
+        tempInt = temp.toInt();
+        description = weatherData["weather"][0]["description"];
+        feelsLike = weatherData["main"]["feels_like"] - 273.15;
+        feelsLikeInt = feelsLike.toInt();
+        windSpeed = (weatherData["wind"]["speed"]) * 3.6;
+        windSpeedInt = windSpeed.toInt();
+        cityName = weatherData["name"];
+        bgColor = weatherData["weather"][0]["icon"];
+        bgColor = bgColor![2];
+        weatherId = weatherData["weather"][0]["id"];
+        imagePicker(weatherId!, bgColor!);
+      });
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   TextEditingController searchCity = TextEditingController();
   @override
   Widget build(BuildContext context) {
@@ -17,123 +82,117 @@ class _WeatherState extends State<Weather> {
       body: Container(
         width: double.infinity,
         height: double.infinity,
-        color: ColorConstants.bgNightColor,
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 70,
-            ),
-            SizedBox(
-              height: 50,
-              width: 350,
-              child: TextField(
-                controller: searchCity,
-                decoration: InputDecoration(
-                    filled: true,
-                    fillColor: ColorConstants.bgDefaultColor,
-                    enabledBorder: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                    ),
-                    focusedBorder: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                    ),
-                    prefixIcon: const Icon(
-                      Icons.search,
-                      size: 30,
-                    ),
-                    hintText: "Enter City",
-                    suffixIcon: IconButton(
-                        onPressed: () {
-                          // City search logic//
-                          print(searchCity.text);
-                        },
-                        icon: const Icon(
-                          Icons.check,
-                          size: 30,
-                        ))),
+        color: bgColor == null
+            ? ColorConstants.bgDefaultColor
+            : bgColor == "d"
+                ? ColorConstants.bgDayColor
+                : bgColor == "n"
+                    ? ColorConstants.bgNightColor
+                    : ColorConstants.bgDefaultColor,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 70,
               ),
-            ),
-            const SizedBox(
-              height: 70,
-            ),
-            Container(
-              width: 250,
-              height: 250,
-              color: ColorConstants.transparentColor,
-              child:
-                  Lottie.asset("assets/animation_widgets/sunny clear day.json"),
-            ),
-            const SizedBox(
-              height: 70,
-            ),
-            Text(
-              "Temperature \u00B0C",
-              style:
-                  TextStyle(color: ColorConstants.bgDefaultColor, fontSize: 30),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Container(
-              width: 150,
-              height: 30,
-              color: ColorConstants.transparentColor,
-              child: Center(
-                child: Text(
-                  "Feels Like 10\u00B0C",
-                  style: TextStyle(
-                      color: ColorConstants.bgDefaultColor, fontSize: 15),
+              SizedBox(
+                height: 50,
+                width: 350,
+                child: TextField(
+                  controller: searchCity,
+                  decoration: InputDecoration(
+                      filled: true,
+                      fillColor: ColorConstants.bgDefaultColor,
+                      enabledBorder: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                      focusedBorder: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                      prefixIcon: const Icon(
+                        Icons.search,
+                        size: 30,
+                      ),
+                      hintText: "Enter City",
+                      suffixIcon: IconButton(
+                          onPressed: () {
+                            String trimmedControllerText =
+                                searchCity.text.trim();
+                            getWeatherData(trimmedControllerText);
+                            setState(() {});
+                          },
+                          icon: const Icon(
+                            Icons.check,
+                            size: 30,
+                          ))),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Container(
-              width: 150,
-              height: 30,
-              color: ColorConstants.transparentColor,
-              child: Center(
-                child: Text(
-                  "Wind speed 10 KM/H",
-                  style: TextStyle(
-                      color: ColorConstants.bgDefaultColor, fontSize: 15),
-                ),
+              const SizedBox(
+                height: 70,
               ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Container(
-                  width: 150,
-                  height: 30,
-                  color: ColorConstants.transparentColor,
-                  child: Center(
-                    child: Text(
-                      "SunRise 6.23 AM",
-                      style: TextStyle(
-                          color: ColorConstants.bgDefaultColor, fontSize: 15),
-                    ),
+              Text(
+                cityName,
+                style: TextStyle(
+                    color: ColorConstants.bgDefaultColor, fontSize: 30),
+              ),
+              Container(
+                width: 250,
+                height: 250,
+                color: ColorConstants.transparentColor,
+                child:
+                    imageShow == "" ? Text("Hello") : Lottie.asset(imageShow),
+              ),
+              const SizedBox(
+                height: 70,
+              ),
+              Text(
+                " $tempInt \u00B0C",
+                style: TextStyle(
+                    color: ColorConstants.bgDefaultColor, fontSize: 30),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                description ?? "",
+                style: TextStyle(
+                    color: ColorConstants.bgDefaultColor, fontSize: 20),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Container(
+                width: 150,
+                height: 30,
+                color: ColorConstants.transparentColor,
+                child: Center(
+                  child: Text(
+                    "Feels Like $feelsLikeInt \u00B0C",
+                    style: TextStyle(
+                        color: ColorConstants.bgDefaultColor, fontSize: 15),
                   ),
                 ),
-                Container(
-                  width: 150,
-                  height: 30,
-                  color: ColorConstants.transparentColor,
-                  child: Center(
-                    child: Text(
-                      "SunRise 6.23 PM",
-                      style: TextStyle(
-                          color: ColorConstants.bgDefaultColor, fontSize: 15),
-                    ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: 150,
+                height: 30,
+                color: ColorConstants.transparentColor,
+                child: Center(
+                  child: Text(
+                    "Wind Speed $windSpeedInt KM/H",
+                    style: TextStyle(
+                        color: ColorConstants.bgDefaultColor, fontSize: 15),
                   ),
                 ),
-              ],
-            )
-          ],
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+            ],
+          ),
         ),
       ),
     );
